af59eb1c17291c1a540caed95347fe38f4be478a
hunter10 <hunter10@naver.com>
2019-01-05 14:06:22 +0900

    부트스트랩 템플릿을 ASP.NET 용 뷰로 변환하는법, (메인페이지, 피드백 페이지만)

--- EddyHomePage/App_Start/BundleConfig.cs
+++ EddyHomePage/App_Start/BundleConfig.cs
@@ -8,23 +8,32 @@ namespace EddyHomePage
         // 묶음에 대한 자세한 내용은 https://go.microsoft.com/fwlink/?LinkId=301862를 참조하세요.
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+            // 스타일시트 추가
+            bundles.Add(new StyleBundle("~/eddyhome/css").Include(
+                   "~/Template/assets/vendor/bootstrap/css/bootstrap.min.css",
+                   "~/Template/assets/vendor/fontawesome/css/font-awesome.min.css",
+                   "~/Template/assets/vendor/flaticons/flaticon.css",
+                   "~/Template/assets/vendor/hover/css/hover-min.css",
+                   "~/Template/assets/vendor/wow/animate.css",
+                   "~/Template/assets/vendor/mfp/css/magnific-popup.css",
+                   "~/Template/assets/vendor/footable/footable.core.css",
+                   "~/Template/assets/custom/css/style.css"));
 
-            bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
-                        "~/Scripts/jquery.validate*"));
-
-            // Modernizr의 개발 버전을 사용하여 개발하고 배우십시오. 그런 다음
-            // 프로덕션에 사용할 준비를 하고 https://modernizr.com의 빌드 도구를 사용하여 필요한 테스트만 선택하세요.
-            bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
-                        "~/Scripts/modernizr-*"));
-
-            bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.js"));
-
-            bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.css",
-                      "~/Content/site.css"));
+            //스크립트파일 추가
+            bundles.Add(new ScriptBundle("~/eddyhome/js").Include(
+            "~/Template/assets/vendor/jquery/js/jquery-2.2.0.min.js",
+            "~/Template/assets/vendor/bootstrap/js/bootstrap.min.js",
+            "~/Template/assets/vendor/imagesloaded/js/imagesloaded.pkgd.min.js",
+            "~/Template/assets/vendor/isotope/js/isotope.pkgd.min.js",
+            "~/Template/assets/vendor/mfp/js/jquery.magnific-popup.min.js",
+            "~/Template/assets/vendor/circle-progress/circle-progress.js",
+            "~/Template/assets/vendor/waypoints/waypoints.min.js",
+            "~/Template/assets/vendor/anicounter/jquery.counterup.min.js",
+            "~/Template/assets/vendor/wow/wow.min.js",
+            "~/Template/assets/vendor/pjax/jquery.pjax.js",
+            "~/Template/assets/vendor/footable/footable.all.min.js",
+            "~/Template/assets/custom/js/custom.js"
+            ));
         }
     }
 }
---
+++ EddyHomePage/Controllers/EddyHomeController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EddyHomePage.Controllers
+{
+    public class EddyHomeController : Controller
+    {
+        // GET: EddyHome
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Feedback()
+        {
+            return View();
+        }
+    }
+}
